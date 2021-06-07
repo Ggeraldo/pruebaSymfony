5275644898bfc11a50cea0cdfe3460da91c1f352
ggeraldo <ggeraldo.ba@gmail.com>
2021-06-07 01:48:03 -0400

    Auth add to project

---
+++ src/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+#use Symfony\Component\Form\Extension\Core\Type\TheChoiceType;
+use Symfony\Component\Form\CallbackTransformer;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+        ;
+
+        $builder->get('roles')
+                ->addModelTransformer(new CallbackTransformer(
+                    function($roleArray){
+                        return count($roleArray) ? $roleArray[0] : null;
+
+                    },
+                    function($roleString){
+                        return [$roleString];
+                    }
+                ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
